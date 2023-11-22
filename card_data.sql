032045b673d451ebea4dd0448341e14aba3d4d57
GuruRathinam <gururathinamrs@gmail.com>
2023-11-22 22:14:41 +0530

    updated the changes

---
+++ card_data.sql
@@ -0,0 +1,11 @@
+create database bizcardx_db;
+
+use bizcardx_db;
+
+CREATE TABLE card_data (company_name TEXT,card_holder TEXT,designation TEXT,mobile_number VARCHAR(50),email TEXT,website TEXT,area TEXT,city TEXT,state TEXT,pin_code VARCHAR(10),image LONGBLOB);
+
+show tables;
+
+select * from card_data;
+
+
